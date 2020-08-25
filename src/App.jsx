df5be0ae6caa17759af69b2d82818691dd9556d8
Nadia080290 <ngallardome@gmail.com>
2020-08-25 12:47:33 -0400

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,14 @@
 import React from 'react';
-// import Inicio from './components/Inicio';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  NavLink
-} from "react-router-dom";
-import Home from './components/Home'
-import PagCuatro from './components/PagCuatro'
+import Inicio from './components/Inicio';
+import Footer from './components/Footer';
+import {Link, withRouter} from 'react-router-dom'
 
 function App() {
-  return (
-    <Router>
-      <div className="App" >
-        <Switch>
-          <Route path="/home">
-            <Home />
-          </Route>
-          <Route path="/pagCuatro">
-            <PagCuatro />
-          </Route>
-        </Switch>
-
-      </div>
-    </Router>
-
+  return ( 
+  <div className="original">
+    <Inicio/>
+    <Footer/>
+    </div>
   );
 }
 
