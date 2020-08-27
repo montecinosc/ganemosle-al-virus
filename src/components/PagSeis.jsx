4ee38540ec4d8e1a28f435a1fd1b5badb90b713a
Nadia080290 <ngallardome@gmail.com>
2020-08-26 23:20:09 -0400

    nueva pagina guantrini

--- src/components/PagSeis.jsx
+++ src/components/PagSeis.jsx
@@ -5,25 +5,24 @@ import '../style/PagSeis.css'
 function PagSeis() {
     return (
         <div>
-             <div className="container">
+            <div className="container">
         
-            <div className="row align-items-center">
-               <div class="col-md-6">
-                    <h5>¡Hola! Soy Guantrini</h5>
-                    <h5>Te invito a conocer todas nuestras aventuras y 
-                        así resolver todas las actividades y desafíos.</h5>
-                    <h3>¡Únete a el Escuadrón Prevención y juntos combatamos el Coronavirus!</h3>
-               </div>
-                 <div class="col-md-6">
+                <div className="row align-items-center">
+                    <div class="col-md-6">
+                            <h5>¡Hola! Soy Guantrini</h5>
+                            <h5>Te invito a conocer todas nuestras aventuras y 
+                                así resolver todas las actividades y desafíos.</h5>
+                            <h3>¡Únete a el Escuadrón Prevención y juntos combatamos el Coronavirus!</h3>
+                    </div>
+                        <div class="col-md-6">
+                    
             
-    
-                 </div>
+                        </div>
+                </div>
+                <img className="guante" alt="patru" src={guantrini}/>
            </div>
-           </div>
-        <div>
-             <img className="guante" alt="patru" src={guantrini}/>
         </div>
-        </div>
+       
         
     )
 }
