15f6905352837c038fd1ca888532d216e3b233a2
Nadia080290 <ngallardome@gmail.com>
2020-08-29 00:04:53 -0400

    avances finales responsive

--- src/components/PagTres.jsx
+++ src/components/PagTres.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import '../style/PagTres.css'
-import corona from '../img/fondoyletra.png' 
+import corona from '../img/fondoyletra8.png'
+import Footer from "./Footer"
+import Header from "./Header" 
+import HeaderCel from '../components/HeaderCel'
 
 function PagTres() {
     return (
-      <div>
-        <div className="corona fondo">
-          
-          <img width="100%"  className="virus" alt="coro" src={corona} />  
-        </div>
+      <div> 
+          <div className="corona fondo">
+          <div className="d-none d-sm-block"><Header/></div>
+          <div className="d-lg-none d-sm-block"><HeaderCel/></div>
+          <img className="virus" alt="coro" src={corona} />  
+          <Footer/>
+          </div>
         </div>
     )
 }
