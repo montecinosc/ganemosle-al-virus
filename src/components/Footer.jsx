1366f8dab98967dd2bd0fd8e849932aee5104df6
Carlareneedc <carla.renee.diazc@gmail.com>
2020-08-29 11:57:18 -0300

    fot

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,6 @@ function Footer() {
     return (
         <div className="footer mt-5">
             <img className="footer" alt="foot" src={footer}/>
-            {/*<div class="row">*/}
-                {/*<div class="col-md-6 morado col-6"></div>*/}
-                {/*<div class="col-md-6 celesteF col-6"></div>*/}
-            {/*</div>*/}
         </div>
     )
 } export default Footer
